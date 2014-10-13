8c679c2e5f86cf73daeb21511c3fe96f3757b092
ueg1990 <uehtesham90@gmail.com>
2014-10-13 06:01:26 -0400

    Initialize module for Coursera Catalog API

---
+++ mooc_aggregator_restful_api/coursera.py
@@ -0,0 +1,34 @@
+'''
+This module retrieves information of all the courses offered by Coursera 
+through their Catalog APIs
+
+Link to documentation:
+https://tech.coursera.org/app-platform/catalog/
+
+'''
+
+import requests
+
+class CourseraAPI(object):
+    '''
+    This class defines attributes and methods for the Coursera Catalog API
+
+    '''
+
+    COURSERA_CATALOG_API_ENDPOINT_COURSES = 'https://api.coursera.org/api/catalog.v1/courses'
+    COURSERA_CATALOG_API_ENDPOINT_UNIVERSITIES = 'https://api.coursera.org/api/catalog.v1/universities'
+    COURSERA_CATALOG_API_ENDPOINT_CATEGORIES = 'https://api.coursera.org/api/catalog.v1/categories'
+    COURSERA_CATALOG_API_ENDPOINT_INSTRUCTORS = 'https://api.coursera.org/api/catalog.v1/instructors'
+    COURSERA_CATALOG_API_ENDPOINT_SESSIONS = 'https://api.coursera.org/api/catalog.v1/sessions'
+
+    def __init__(self):
+        self.response_courses = requests.get(CourseraAPI.COURSERA_CATALOG_API_ENDPOINT_COURSES)
+        self.response_universities = requests.get(CourseraAPI.COURSERA_CATALOG_API_ENDPOINT_UNIVERSITIES)
+        self.response_categories = requests.get(CourseraAPI.COURSERA_CATALOG_API_ENDPOINT_CATEGORIES)
+        self.response_instructors = requests.get(CourseraAPI.COURSERA_CATALOG_API_ENDPOINT_INSTRUCTORS)
+        self.response_sessions = requests.get(CourseraAPI.COURSERA_CATALOG_API_ENDPOINT_SESSIONS)
+
+    
+
+if __name__ == '__main__':
+    pass
