79f92d050fbf9ebe4f088aeabb5e832abeefe0d5
ueg1990 <uehtesham90@gmail.com>
2014-10-14 00:37:20 -0400

    Initialize unit tests for Coursera API module

---
+++ tests/test_coursera.py
@@ -0,0 +1,34 @@
+import unittest
+
+from mooc_aggregator_restful_api import coursera
+
+
+class CourseraTestCase(unittest.TestCase):
+    '''
+    Unit Tests for module udacity
+
+    '''
+
+    def setUp(self):
+        self.coursera_test_object = coursera.CourseraAPI()
+
+    def test_coursera_api_courses_response(self):
+        self.assertEqual(self.coursera_test_object.response_courses.status_code, 200)
+
+    def test_coursera_api_universities_response(self):
+        self.assertEqual(self.coursera_test_object.response_universities.status_code, 200)
+
+    def test_coursera_api_categories_response(self):
+        self.assertEqual(self.coursera_test_object.response_categories.status_code, 200)
+
+    def test_coursera_api_instructors_response(self):
+        self.assertEqual(self.coursera_test_object.response_instructors.status_code, 200)
+
+    def test_coursera_api_sessions_response(self):
+        self.assertEqual(self.coursera_test_object.response_sessions.status_code, 200)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
