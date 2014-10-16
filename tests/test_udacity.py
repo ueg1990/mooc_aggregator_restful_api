d565fdab9cefc080ff3127f036c19e95cba73f6e
ueg1990 <uehtesham90@gmail.com>
2014-10-16 01:22:01 -0400

    Add unit test for mongofy_courses of udacity module

--- tests/test_udacity.py
+++ tests/test_udacity.py
@@ -15,6 +15,10 @@ class UdacityTestCase(unittest.TestCase):
     def test_udacity_api_response(self):
         self.assertEqual(self.udacity_test_object.status_code(), 200)
 
+    def test_udacity_api_mongofy_courses(self):
+        course = self.udacity_test_object.mongofy_courses()[0]
+        self.assertEqual(course['title'], 'Intro to Computer Science')
+    
     def tearDown(self):
         pass
 
